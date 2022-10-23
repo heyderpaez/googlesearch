aceb750905b79b86d471669a299645e2d26371d3
heyderpaez <heyderpaez@gmail.com>
2022-10-22 21:06:17 -0500

    arreglo del webdriver de chrome

--- sortgs.py
+++ sortgs.py
@@ -135,6 +135,9 @@ def setup_driver():
     print('Loading...')
     chrome_options = Options()
     chrome_options.add_argument("disable-infobars")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
