5e220142512118f3bb70d688715df6cdfb38d848
heyderpaez <heyderpaez@gmail.com>
2022-10-22 20:50:13 -0500

    Update sortgs.py

--- sortgs.py
+++ sortgs.py
@@ -135,7 +135,7 @@ def setup_driver():
     print('Loading...')
     chrome_options = Options()
     chrome_options.add_argument("disable-infobars")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     return driver
 
 def get_author(content):
